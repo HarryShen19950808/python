e2f9c1f8ca125499c9000e06f0003216b3be1435
HarryShen <ms024929548@gmail.com>
2018-11-16 16:50:14 +0800

    2018_11_16_4

--- scikit-fuzzy/fuzzy_test.py
+++ scikit-fuzzy/fuzzy_test.py
@@ -116,9 +116,10 @@ water_temp_activation = fuzz.interp_membership(y_temp, aggregated, water_temp)  
 # Visualize this
 fig, ax0 = plt.subplots(figsize=(8, 3))
 
-ax0.plot(y_temp, water_cool, 'b', linewidth=0.5, linestyle='--', )
-ax0.plot(y_temp, water_warm, 'g', linewidth=0.5, linestyle='--')
-ax0.plot(y_temp, water_hot, 'r', linewidth=0.5, linestyle='--')
+ax0.plot(y_temp, water_cool, 'y', linewidth=0.5, linestyle='--', )
+ax0.plot(y_temp, water_warm, 'b', linewidth=0.5, linestyle='--')
+ax0.plot(y_temp, water_hot, 'g', linewidth=0.5, linestyle='--')
+ax0.plot(y_temp, water_very_hot, 'r', linewidth=0.5, linestyle='--')
 ax0.fill_between(y_temp, tip0, aggregated, facecolor='Orange', alpha=0.7)
 ax0.plot([water_temp, water_temp], [0, water_temp_activation], 'k', linewidth=1.5, alpha=0.9)
 ax0.set_title('Aggregated membership and result (line)')
@@ -131,4 +132,4 @@ for ax in (ax0,):
     ax.get_yaxis().tick_left()
 
 plt.tight_layout()
-print("The water temperature is " + str(water_temp))
+print("The water temperature is " + str(water_temp) + " ℃")
